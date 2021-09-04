aa7598ff87d17049417a021d67f27afa569934ab
Perondas <Pperondas@gmail.com>
2021-09-04 13:18:38 +0200

    Implemented the start menu

---
+++ Minesweeper.View/Windows/StartMenu.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Minesweeper.Common.Data;
+
+namespace Minesweeper.View.Windows
+{
+    /// <summary>
+    /// Interaction logic for StartMenu.xaml
+    /// </summary>
+    public partial class StartMenu : Window
+    {
+        private string hCells;
+        private string vCells;
+        private string mines;
+
+        public StartMenu()
+        {
+            this.hCells = "";
+            this.vCells = "";
+            this.mines = "";
+            InitializeComponent();
+        }
+
+        public GameSettings? Display ()
+        {
+            this.ShowDialog();
+
+            return !int.TryParse(this.hCells, out var hResult)
+                ? null
+                : !int.TryParse(this.vCells, out var vResult)
+                    ? null
+                    : !int.TryParse(this.mines, out var mResult)
+                        ? (GameSettings?) null
+                        : new GameSettings(hResult, vResult, mResult);
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.hCells = this.HCells.Text;
+            this.vCells = this.VCells.Text;
+            this.mines = this.Mines.Text;
+            this.Close();
+        }
+    }
+}
